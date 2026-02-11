90327cb6d2402d6faa0973b3fbf8e60ff44e54b1
hayato ojima <hojima@kashiwa.nagaokaut.ac.jp>
2026-02-12 07:06:43 +0900

    gitignoreに/libが含まれていたのを修正

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,20 @@
+export const FOCUS_TRACKS = [
+    "/music/Focus/Barely.mp3",
+    "/music/Focus/Dull.mp3",
+    "/music/Focus/Epoch.mp3",
+    "/music/Focus/Fake.mp3",
+    "/music/Focus/Notion.mp3",
+    "/music/Focus/Something.mp3",
+    "/music/Focus/Teleport.mp3",
+    "/music/Focus/Trudge.mp3",
+    "/music/Focus/Wired.mp3"
+];
+
+export const BREAK_TRACKS = [
+    "/music/Break/Arrival.mp3",
+    "/music/Break/Daylight.mp3",
+    "/music/Break/Everyday.mp3",
+    "/music/Break/Little More.mp3",
+    "/music/Break/Sanctuary.mp3",
+    "/music/Break/Toy.mp3"
+];
